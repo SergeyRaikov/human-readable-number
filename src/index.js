95cffef29fde3aef2100283994b72123d41a7328
Sergey <rajkovsergej81@gmail.com>
2023-01-15 21:15:58 +0300

    init: start task

--- src/index.js
+++ src/index.js
@@ -1,35 +1,58 @@
-module.exports = function toReadable (number) {
-    const fromZeroToNineteen ={
-        1: 'one',
-        2: 'two',
-        3: 'three',
-        4: 'four',
-        5: 'five',
-        6: 'six',
-        7: 'seven',
-        8: 'eight',
-        9: 'nine',
-        10: 'ten',
-        11: 'elleven',
-        12: 'twelve',
-        13: 'thirteen'
-        14: 'fourteen',
-        15: 'fifteen',
-        16: 'sixteen',
-        17: 'seventeen',
-        18: 'eighteen',
-        19: 'nineteen',
-        20: 'twenty',
-    }
+module.exports = function toReadable(number) {
+  const units = [
+      'zero',
+      'one',
+      'two',
+      'three',
+      'four',
+      'five',
+      'six',
+      'seven',
+      'eight',
+      'nine',
+      'ten',
+      'eleven',
+      'twelve',
+      'thirteen',
+      'fourteen',
+      'fifteen',
+      'sixteen',
+      'seventeen',
+      'eighteen',
+      'nineteen',
+  ];
+  const tens = [
+      '',
+      '',
+      'twenty',
+      'thirty',
+      'forty',
+      'fifty',
+      'sixty',
+      'seventy',
+      'eighty',
+      'ninety',
+  ];
+
+  const hundreds = Math.floor(number / 100);
+  const tensOfNumber = number - hundreds * 100;
+  const penultimateDigit = Math.floor(tensOfNumber / 10);
+  const lastDigit = Math.floor(tensOfNumber % 10);
+
   let result = '';
-   const numArr =  number.toString().split('');
-  for (let i = 0; i < numArr.length; i++) {
-    const element = numArr[i];
-    if (Number(element) === 0)
-    result += fromZeroToNineteen[element];
+
+  if (number === 0) return units[number];
+
+  if (hundreds) result = units[hundreds] + ' hundred';
+
+  if (tensOfNumber <= 19) {
+     if (tensOfNumber !== 0) result += (hundreds > 0 ? ' ' : '') + units[tensOfNumber];
+  } else {
+      result += (hundreds > 0 ? ' ' : '') + tens[Math.floor(penultimateDigit)];
+      if (lastDigit) {
+        result += ' ' + units[lastDigit]
+      }             
   }
+ 
   return result;
-}
-
-
-
+};
